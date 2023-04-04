53911c597094d7fd176f0d1a4d3656e973d94e70
MercyAnna30 <gnosis365000@gmail.com>
2023-04-04 00:37:10 -0700

    string appending

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,14 +6,6 @@
  *
  * Return: void
  */
-#include "main.h"
-/**
- * _strcat - concatenates two strings
- * @dest: input value
- * @src: input value
- *
- * Return: void
- */
 char *_strcat(char *dest, char *src)
 {
 	int i;
